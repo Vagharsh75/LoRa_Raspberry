23f4c9e6689dfc161fe4442def456827e0d3f5dc
Vagharsh75 <vagharsh75@gmail.com>
2020-06-20 19:37:41 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <src/LoRa_RasPi.h>
+byte hello[32] = "HELLO";
+
+
+int main (int argc, char *argv[]) {
+	if (argc < 2) {
+        printf ("Usage: argv[0] sender|rec [message]\n");
+        exit(1);
+    }
+    
+	LoRa MyLora;
+	MyLora.begin(433E6);
+	if (!strcmp("sender", argv[1])) {
+		MyLora.configTransmitter();
+        printf("Send packets at SF%i on %.6lf Mhz.\n", MyLora.getSF(),(double)(MyLora.getLoRaFreq())/1000000);
+        printf("------------------\n");
+
+        if (argc > 2)
+            strncpy((char *)hello, argv[2], sizeof(hello));
+
+        while(1) {
+            MyLora.txlora(hello, strlen((char *)hello));
+            delay(5000);
+        }
+    } else {
+
+        // radio init
+        MyLora.configReceiver();
+        printf("Listening at SF%i on %.6lf Mhz.\n", MyLora.getSF(),(double)(MyLora.getLoRaFreq())/1000000);
+        printf("------------------\n");
+        while(1) {
+            MyLora.receivepacket(); 
+            delay(1);
+        }
+
+    }
+	
+	
+	return (0);
+}
